acfd40feb755b62ed7a50930e543a128a98a79fa
malik <malik.h@webdevpro.net>
2019-10-24 14:12:04 +0200

    annotations

--- router/utilisateurs.js
+++ router/utilisateurs.js
@@ -14,40 +14,48 @@ router.get("/", (req, resp) => {
 // POST
 // { "login" : "test@yahoo.fr" , "mdp" : "azerty" , "role" : "admin"}
 router.post("/", (req, resp) => {
+  //1
   validation(req.body)
     .then(() => {
+      //2 si OK
       // vérifier que les données transmises sont conformes
 
       // si ok => je passe à l'étape suivante
 
       // vérifier que le login (email) n'existe pas déjà ?
       Utilisateur.find({ login: req.body.login }).then(result => {
-        //console.log(result);
+        // 3 soit j'ai [....]
+        // 3 soit j'ai []
         if (result.length !== 0)
+          // 3' si j'ai un tableau plein alors je sors de la post => 400
           return resp.status(400).send("login existe déjà !");
         // si ok => je passe à l'étape suivante
         // la création du profil
 
         //Crypter le mot de passe
         // ralentir le hacker pour le vol d'identité
-
+        // 4 générer un salt = compliquer le mot de passe  = azerty + fdoezhfpezohuoihfzhu
         bcrypt.genSalt(10).then(salt => {
+          //5 req.body.mdp que je salt => hashedPassword
           bcrypt.hash(req.body.mdp, salt).then(hashedPassword => {
-            //créer le profil avec un mot de passe hashé !
+            // 6 créer le profil avec un mot de passe hashé !
             const utilisateur = new Utilisateur({
               login: req.body.login,
               mdp: hashedPassword,
               role: req.body.role
             });
 
-            utilisateur.save().then(() => {
-              resp.send("nouvel utilisateur créé !");
+            // 7 create mon profil
+            utilisateur.save().then(nouveauCompte => {
+              // 8 retourner le nouveau compte avec { _id:"foizhfoezhi"}
+              resp.send(nouveauCompte);
             });
           });
         });
       });
     })
     .catch(error => {
+      // 2 si KO
       resp.status(400).send(error.details[0].message);
     });
 });
