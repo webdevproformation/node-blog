6c26e3ebfa36ba870b511ec5b1ed4b96ee5be05f
malik <malik.h@webdevpro.net>
2019-10-25 17:09:44 +0200

    ajout cors / modification de la sortie de connexion

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const articlesRouter = require("./router/articles");
-const connexionRouter = require("./router/connexion");
-const utilisateurRouter = require("./router/utilisateurs");
-const express = require("express");
-const mongoose = require("mongoose");
+const articlesRouter = require('./router/articles');
+const connexionRouter = require('./router/connexion');
+const utilisateurRouter = require('./router/utilisateurs');
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use("/api/articles", articlesRouter);
-app.use("/api/utilisateurs", utilisateurRouter);
-app.use("/api/connexion", connexionRouter);
+app.use(cors());
+app.use('/api/articles', articlesRouter);
+app.use('/api/utilisateurs', utilisateurRouter);
+app.use('/api/connexion', connexionRouter);
 
 mongoose
-  .connect("mongodb://localhost/blog", {
+  .connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true
   })
 
   .then(function() {
-    console.log("Connecté à la base blog MongoDB");
+    console.log('Connecté à la base blog MongoDB');
   })
   .catch(function(err) {
     console.log(new Error(err));
--- router/connexion.js
+++ router/connexion.js
@@ -6,10 +6,10 @@
 // si c'est pas conforme => retour c'est pas conforme
 // si c'est conforme => jwt
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Utilisateur } = require("../model/utilisateurs");
-const bcrypt = require("bcrypt");
+const { Utilisateur } = require('../model/utilisateurs');
+const bcrypt = require('bcrypt');
 
 //Postman
 //http://localhost:5000/api/connexion
@@ -17,7 +17,7 @@ const bcrypt = require("bcrypt");
 // {"login" : "login7@yahoo.fr" , "mdp" : "azerty"}
 // credentials
 
-router.post("/", (req, resp) => {
+router.post('/', (req, resp) => {
   //req.body.login; // contient  {"login" : "login7@yahoo.fr" , "mdp" : "azerty"}
 
   // 1 login qui a été envoyé est conforme => existe dans Mongo
@@ -25,16 +25,16 @@ router.post("/", (req, resp) => {
   Utilisateur.find({ login: req.body.login }).then(result => {
     //console.log(result);
     if (result.length === 0)
-      return resp.status(400).send("login ou mot de passe incorrect");
+      return resp.status(400).send('login ou mot de passe incorrect');
 
     // 2 mdp qui a été envoyé est conforme
     bcrypt.compare(req.body.mdp, result[0].mdp).then(validPassword => {
       if (!validPassword)
-        return resp.status(400).send("login ou mot de passe incorrect");
+        return resp.status(400).send('login ou mot de passe incorrect');
 
       // envoyer jwt
       const token = result[0].generateAuthenToken();
-      resp.header("x-auth", token).send("Bienvenu dans le Back Office !");
+      resp.header('x-auth', token).send({ token: token });
     });
   });
 });
